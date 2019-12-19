99b6eda757ff5d0e397659d7abb5a021af5e1852
SJ <sjisyours@gmail.com>
2019-12-19 12:39:39 +0900

    update sort by categories component as 'showPostsByCategory()' in BlogList

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,49 +1,65 @@
 import Link from "next/link";
 import ReactMarkdown from "react-markdown";
+import {Fragment} from "react";
 
 const BlogList = (props) => {
 
-  const sortByDateAcs =(props) =>{
-    var allBlogs = props.allBlogs;
-    allBlogs.sort((a,b)=>{
-      if(a.document.data.date > b.document.data.date) return -1
-      if(a.document.data.date < b.document.data.date) return 1
-      return 0
-    })
+    const sortByDateAcs = (props) => {
+        var allBlogs = props.allBlogs;
+        allBlogs.sort((a, b) => {
+            if (a.document.data.date > b.document.data.date) 
+                return -1
+            if (a.document.data.date < b.document.data.date) 
+                return 1
+            return 0
+        })
+    }
 
-  }
+    const showPostsByCategory = (props, category) => {
+        var category = category;
+        return (
+            <ul className="list">
+                <h1 className="category-title">{category}</h1>
+                {props.allBlogs.length > 1 && props
+                    .allBlogs
+                    .map((post, i) => {
+                        {
+                            if(post.document.data.category == category) {
+                                return (
+                                    <Fragment key={i}>
+                                        <Link
+                                            key={post.slug}
+                                            href={{
+                                            pathname: `/blog/${post.slug}`
+                                        }}>
+                                            <a>
+                                                <li>
+                                                    <div className="blog__info">
+                                                        <h2 className='post-title'>{post.document.data.title}</h2>
+                                                    </div>
+                                                </li>
+                                            </a>
+                                        </Link>
+                                    </Fragment>
+                                )
+                            }
+                        }
+                    })
+}
+            </ul >
+        )
 
-  function truncateSummary(content) {
-    return content.slice(0, 200).trimEnd();
-  }
+    }
 
-  function reformatDate(fullDate) {
-    const date = new Date(fullDate)
-    return date.toDateString().slice(4);
-  }
-
-  return (
-    <>
-      {sortByDateAcs(props) }
-      <ul className="list">
-        {props.allBlogs.length > 1 && props.allBlogs.map(post => (
-          <Link
-            key={post.slug}
-            href={{ pathname: `/blog/${post.slug}` }}
-          >
-            <a>
-            <li>
-              <div className="blog__info">
-                <h2 className='post-title'>{post.document.data.title}</h2>
-              </div>
-            </li>
-            </a>
-          </Link>
-        ))}
-      </ul>
-   
-    </>
-  );
+    return (
+        <Fragment>
+            {sortByDateAcs(props)}
+            {showPostsByCategory(props, "Coding")}
+            {showPostsByCategory(props, "Book")}
+            {showPostsByCategory(props, "Essay")}
+            {showPostsByCategory(props, "Marketing")}
+        </Fragment >
+    );
 };
 
 export default BlogList;
